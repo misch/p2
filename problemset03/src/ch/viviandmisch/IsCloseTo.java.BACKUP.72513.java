05c9dfab569de402f8acca6589e981033546648f
Viviane Tanner <vita@students.unibe.ch>
2011-04-05 18:48:11 +0200

    Solved problemset06.
    Wrote down 1 or 2 hypotheses for each failing test and checked them.
    Worked out some coding rules.
    	modified:   coding-conventions.mdown
    	modified:   hypotheses.mdown
    	new file:   img/Add()_inserts_object_at_right_Index.tiff
    	new file:   img/Add_inserts_object_at_right_Index.tiff
    	new file:   img/addAll_computes_wrong_index.tiff
    	new file:   img/addAll_works_right.tiff
    	new file:   img/addAt_inserts_at_wrong_index.tiff
    	new file:   img/get(0)_returns_13.tiff
    	new file:   img/get(0)_returns_wrong_item_after_addAll.tiff
    	new file:   img/get(0)_returns_wrong_object.tiff
    	new file:   img/get_0_returns_13.tiff
    	new file:   img/get_0_returns_wrong_item_after_addAll.tiff
    	new file:   img/get_0_returns_wrong_object.tiff
    	new file:   img/it.next_exists_but_is_null(2).tiff
    	new file:   img/it.next_exists_but_is_null.tiff
    	new file:   img/it.next_exists_but_is_null_2.tiff
    	new file:   img/remove_removes_wrong_item.tiff

---
+++ problemset03/src/ch/viviandmisch/IsCloseTo.java.BACKUP.72513.java
@@ -0,0 +1,39 @@
+package ch.viviandmisch;
+
+import java.awt.geom.Point2D;
+
+import org.hamcrest.Description;
+import org.hamcrest.Factory;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+<<<<<<< HEAD
+/*DR Accepted! But try do exactly read the task!
+ * 
+ * Everything was well done (responsibilities and matcher are ok)
+ */
+=======
+
+>>>>>>> 011c51fe3791178f13f485f325d898a97d1ec3b1
+public class IsCloseTo extends TypeSafeMatcher<Point2D>{
+
+	private Point2D point;
+
+	public IsCloseTo(Point2D point){
+		this.point = point;
+	}
+	
+	@Override
+	protected boolean matchesSafely(Point2D item) {
+		return (item.distance(point)<0.01);
+	}
+
+	@Override
+	public void describeTo(Description description) {
+		description.appendText("is close to " + point.toString());
+	}
+	
+	@Factory
+	public static <T> Matcher<Point2D> closeTo(Point2D point) {
+	    return new IsCloseTo(point);
+	}
+}
